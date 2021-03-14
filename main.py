a2125307ea76374ef9c8f57fb2d8f730ff86a443
alyssaurbanski23 <831554@mystma.org>
2021-03-14 20:46:01 +0000

    planets try 2

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def planets(neptune):
-    return word(planet)
+def planets(p):
+#def myList()
+    for i in range(0, len(planet)):
+        if p == planet[i]:
+            return i + 1
+    return p + " is not a planet" 
+#mylist = [1, 2, 3, 4, 5, 6, 7, 8]
+#myList.append(2)
+planet = ["mercury", "venus", "earth", "mars", "jupitur", "saturn", "uranus", "neptun"]
+print (planets("earth"))
 
-
-print(planets(neptune))
+#"mercury" = 1; "venus" = 2; "earth" = 3
